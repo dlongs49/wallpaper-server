cf793072e322a4beb7b1b36face3a217c5fa9c8f
dinglong <dillonl49@163.com>
2024-01-02 17:25:34 +0800

    chore:#增加passport-local插件

--- src/server/app/app.controller.ts
+++ src/server/app/app.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import {ApiTags} from "@nestjs/swagger";
 @ApiTags("用户管理")
-@Controller("/user")
+@Controller("sign")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("/reg_login")
   getHello(): string {
     return this.appService.getHello();
   }
