5ae4a1585506cc4558d111c778940979aaec6e9a
dinglong <dillonl49@163.com>
2024-01-03 22:01:44 +0800

    feat:#到处sql文件

--- db/wallpaper_db.sql
+++ db/wallpaper_db.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : wallpaper
-Source Server Version : 50726
+Source Server Version : 80012
 Source Host           : localhost:3306
 Source Database       : wallpaper_db
 
 Target Server Type    : MYSQL
-Target Server Version : 50726
+Target Server Version : 80012
 File Encoding         : 65001
 
-Date: 2024-01-03 17:23:47
+Date: 2024-01-03 22:01:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,9 +27,6 @@ CREATE TABLE `user` (
   `reg_time` datetime DEFAULT NULL,
   `sex` varchar(255) DEFAULT NULL,
   `sex_tit` varchar(255) DEFAULT NULL,
+  `nation` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of user
--- ----------------------------
