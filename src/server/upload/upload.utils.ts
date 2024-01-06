e44012829fa5f1d7854a8e3099dbb6154845e18b
dinglong <dillonl49@163.com>
2024-01-06 17:56:14 +0800

    feat:#增加静态资源处理

---
+++ src/server/upload/upload.utils.ts
@@ -0,0 +1,38 @@
+import dayjs from "dayjs";
+import * as fs from "fs";
+import { join } from "path";
+import {v4 as uid} from "uuid";
+import { ResFail } from "../../utils/http.response";
+let file_type = ["image/png", "image/jpeg", "image/gif"];
+// 配置文件路径
+export const HandleDestination = (req, file, cb) => {
+  let date = dayjs(new Date()).format("YYYY-MM-DD");
+  // 检查是否存在该目录
+  const isdir = fs.existsSync(join(__dirname, "../../../www/public/avatar/" + date));
+  let dir_path = join(__dirname, "../../../www/public/avatar/" + date);
+  if (!isdir) {
+    // 创建目录
+    fs.mkdirSync(dir_path);
+  }
+  return cb(null, dir_path);
+};
+// 配置文件名称
+export const HandleFilename = (req, file, callback) => {
+  const file_origin = file.originalname.split('.');
+  const file_name = uid()+'.'+file_origin[1]
+  callback(null, file_name);
+};
+// 校验文件
+export const HandleFileFilter = (req, file, cb) => {
+  if (!file_type.includes(file.mimetype)) {
+    return cb(new ResFail("上传图片格式有误"), false);
+  }
+  cb(null, true);
+};
+// 返回路径
+export const JsonFile = (files)=>{
+  let files_path = files.path.replace(/\\/g,"/")
+  let i = files_path.indexOf(files.fieldname)
+  let url = '/upload' + files_path.slice(i-1,files_path.length)
+  return {url,name:files.originalname}
+}
