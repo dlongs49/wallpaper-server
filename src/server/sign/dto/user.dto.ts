960aa902f29412fd26993c0ed61b3dbf44b04b0a
dinglong <dillonl49@163.com>
2024-01-06 23:41:53 +0800

    feat:#增加更新个人信息dto

---
+++ src/server/sign/dto/user.dto.ts
@@ -0,0 +1,24 @@
+/*
+* Author:dinglong
+* DateTime:2024/1/6 下午 10:54
+*/
+import {ApiProperty, ApiResponse} from "@nestjs/swagger";
+
+export class UserDto{
+    @ApiProperty({
+        description: '头像',
+    })
+    avatar: string;
+    @ApiProperty({
+        description: '昵称',
+    })
+    uname: string;
+    @ApiProperty({
+        description: '性别',
+    })
+    sex: string;
+}
+export class  ArticleListResponse{
+    @ApiProperty({ description: '状态码', example: 200, })
+    code: number
+}
