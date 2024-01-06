19bb9ef15b623b67422d8aecb70afa23edf82d96
dinglong <dillonl49@163.com>
2024-01-07 00:41:37 +0800

    feat:#增加壁纸类型dto

---
+++ src/server/wallpaper/dto/wallpaper_type.dto.ts
@@ -0,0 +1,20 @@
+/*
+* Author:dinglong
+* DateTime:2024/1/7 上午 12:34
+*/
+import {ApiProperty} from "@nestjs/swagger";
+
+export class WallpaperTypeReqDto {
+    @ApiProperty({
+        description: '壁纸类型标题',
+    })
+    title: string;
+    @ApiProperty({
+        description: '壁纸展示封面图片',
+    })
+    cover_url: string;
+    @ApiProperty({
+        description: '壁纸链接类型 0 上传 1 链接',
+    })
+    cover_type: number;
+}
