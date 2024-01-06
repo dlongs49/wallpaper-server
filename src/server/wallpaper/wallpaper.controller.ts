8d611d481e535f534d6f8c977f5a39dd69313f71
dinglong <dillonl49@163.com>
2024-01-07 00:41:42 +0800

    feat:#增加壁纸类型dto

--- src/server/wallpaper/wallpaper.controller.ts
+++ src/server/wallpaper/wallpaper.controller.ts
@@ -1,13 +1,14 @@
-import {Controller, Inject, Post} from '@nestjs/common';
+import {Body, Controller, Inject, Post} from '@nestjs/common';
 import {ApiOperation, ApiTags} from "@nestjs/swagger";
 import {WallpaperService} from "./wallpaper.service";
+import {WallpaperTypeReqDto} from "./dto/wallpaper_type.dto";
 @ApiTags("壁纸管理")
 @Controller('wallpaper')
 export class WallpaperController {
     constructor(private wallpaperService:WallpaperService, ) {}
     @ApiOperation({ summary: '新增壁纸类型' })
     @Post("set_wallpaper_type")
-    setWallpaperType(){
-
+    setWallpaperType(@Body() wallpaperTypeResDto: WallpaperTypeReqDto){
+        return this.wallpaperService.setWallpaperType(wallpaperTypeResDto)
     }
 }
--- src/server/wallpaper/wallpaper.service.ts
+++ src/server/wallpaper/wallpaper.service.ts
@@ -1,7 +1,11 @@
 import {Inject, Injectable} from '@nestjs/common';
+import {WallpaperTypeReqDto} from "./dto/wallpaper_type.dto";
 
 @Injectable()
 export class WallpaperService {
-    // constructor(@Inject("WALLPAPER_TYPEE_PROVIDERS") private  readonly wallpaper_type_providers: any) {
-    // }
+    constructor(@Inject("WALLPAPER_TYPEE_PROVIDERS") private  readonly wallpaper_type_providers: any) {
+    }
+    setWallpaperType(wallpaperTypeResDto: WallpaperTypeReqDto){
+        this.wallpaper_type_providers.create()
+    }
 }
