4dd04e012a21e793be4d6b9f19205240b59d971a
dinglong <dillonl49@163.com>
2024-01-07 21:56:40 +0800

    feat:#增加若干文件

---
+++ client/src/routes/index.js
@@ -0,0 +1,10 @@
+/*
+* Author:dinglong
+* DateTime:2024/1/7 下午 9:54
+*/
+import wallpaper_type from "../lib/wallpaper_type/index.svelte";
+import login from "../lib/login/index.svelte"
+export default {
+    "/":login,
+    "/wallpaper_type":wallpaper_type
+}
