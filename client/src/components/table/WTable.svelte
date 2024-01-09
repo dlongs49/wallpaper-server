01382a0f773ee232d00b1b2ef5da28a1e225238f
dinglong <dillonl49@163.com>
2024-01-09 21:32:28 +0800

    chore:🛠️完成图片预览 [client]

--- client/src/components/table/WTable.svelte
+++ client/src/components/table/WTable.svelte
@@ -1,60 +1,69 @@
 <script>
-  export let data = [];
-  export let column = [];
+    export let data = [];
+    export let column = [];
 </script>
 <div class="wtable">
-  <table>
-    <colgroup style="width:100%">
-      {#each column as cell}
-        <col width={cell.width}>
-      {/each}
-    </colgroup>
-    <thead>
-    <tr>
-      {#each column as item}
-        <th>
-          <div>{item.title}</div>
-        </th>
-      {/each}
-    </tr>
-    </thead>
-    <tbody>
-    {#each data as item}
-      <tr>
-        {#each column as cell,i}
-          <td>
-            {#if i === 1}
-              <div class="cell">
-                <slot name="cell_1" prop={item}></slot>
-              </div>
-            {:else if i === 2}
-              <div class="cell">
-                <slot name="cell_2" prop={item}></slot>
-              </div>
-            {:else if i === 3}
-              <div class="cell">
-                <slot name="cell_3" prop={item}></slot>
-              </div>
-            {:else if i === 4}
-              <div class="cell">
-                <slot name="cell_4" prop={item}></slot>
-              </div>
-            {:else}
-              <div class="cell">{item[cell.key]}</div>
-            {/if}
-          </td>
+    <table>
+        <colgroup style="width:100%">
+            {#each column as cell}
+                <col width={cell.width}>
+            {/each}
+        </colgroup>
+        <thead>
+        <tr>
+            {#each column as item}
+                <th>
+                    <div>{item.title}</div>
+                </th>
+            {/each}
+        </tr>
+        </thead>
+        <tbody>
+        {#each data as item}
+            <tr>
+                {#each column as cell,i}
+                    <td>
+                        {#if i === 1}
+                            <div class="cell">
+                                <slot name="cell_1" prop={item}></slot>
+                            </div>
+                        {:else if i === 2}
+                            <div class="cell">
+                                <slot name="cell_2" prop={item}></slot>
+                            </div>
+                        {:else if i === 3}
+                            <div class="cell">
+                                <slot name="cell_3" prop={item}></slot>
+                            </div>
+                        {:else if i === 4}
+                            <div class="cell">
+                                <slot name="cell_4" prop={item}></slot>
+                            </div>
+                        {:else}
+                            <div class="cell">{item[cell.key]}</div>
+                        {/if}
+                    </td>
+                {/each}
+            </tr>
         {/each}
-      </tr>
-    {/each}
-    </tbody>
-  </table>
-
+        </tbody>
+    </table>
+    {#if data.length === 0}
+        <div class="wtable-nodata">暂无数据</div>
+    {/if}
 </div>
 <style lang="less">
   .wtable {
     width: 100%;
     border: 1px solid #f2f2f2;
     margin-top: 20px;
+
+    .wtable-nodata {
+      height: 100px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
 
     table {
       width: 100%;
