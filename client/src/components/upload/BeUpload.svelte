6b133f561bf32bcb80cc170a01a7a67fbac1d422
dinglong <dillonl49@163.com>
2024-01-09 12:21:03 +0800

    feat:完成图片编辑回显样式[client]

--- client/src/components/upload/BeUpload.svelte
+++ client/src/components/upload/BeUpload.svelte
@@ -1,38 +1,46 @@
 <script>
-    import {BeIcon, BeUpload} from "@brewer/beerui";
-    import { onMount } from "svelte";
-    let base_url = import.meta.env.VITE_APP_BASE_URL
-    let action = base_url + "/api/upload/wallpaper"
-    let fileList = []
-    export let imgUrl
-    export let onUrl = () => {}
-    onMount(()=>{
-        setTimeout(()=>{
-            fileList = [
-                {uid:new Date().getTime().toString(),url:base_url+imgUrl,name:""}
-            ]
-        },500)
-    })
+  import { BeIcon, BeUpload } from "@brewer/beerui";
+  import { onMount } from "svelte";
+
+  let base_url = import.meta.env.VITE_APP_BASE_URL;
+  let action = base_url + "/api/upload/wallpaper";
+  let fileList = [];
+  export let imgUrl;
+  export let onUrl = () => {
+  };
+  onMount(() => {
+    setTimeout(() => {
+      fileList = [
+        { uid: new Date().getTime().toString(), url: base_url + imgUrl, name: "" }
+      ];
+    }, 500);
+  });
 </script>
 <main>
-    <BeUpload
-            action={action}
-            multiple
-            name="wallpaper"
-            accept="image/jpeg,image/png"
-            listType="picture-card"
-            limit={1}
-            hideUpload={true}
-            onSuccess={onUrl}
-            fileList={fileList}>
-        <div class="upload-card">
-            <BeIcon name="add" width="40" height="40"/>
-        </div>
-    </BeUpload>
+  <BeUpload
+    action={action}
+    multiple
+    name="wallpaper"
+    accept="image/jpeg,image/png"
+    listType="picture-card"
+    limit={1}
+    hideUpload={true}
+    onSuccess={onUrl}
+    fileList={fileList}>
+    <div class="upload-card">
+      <BeIcon name="add" width="40" height="40" />
+    </div>
+  </BeUpload>
 </main>
 <style lang="less">
   :global(.be-upload--picture-card) {
     width: 100px !important;
     height: 100px !important;
+  }
+
+  :global(.be-upload-list__item) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 </style>
