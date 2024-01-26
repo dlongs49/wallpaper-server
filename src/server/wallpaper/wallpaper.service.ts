12c2f52a6abf4e55af2b84ed9a2e7ed46ba9b2c6
dinglong <dillonl49@163.com>
2024-01-26 22:59:19 +0800

    perf:🚀解优化壁纸类型返回条数[server]

--- src/server/wallpaper/wallpaper.service.ts
+++ src/server/wallpaper/wallpaper.service.ts
@@ -96,7 +96,6 @@ export class WallpaperService {
       include: [
         {
           model: WallpaperModel,
-          duplicating:false,
           attributes: [
             "type_id"
           ]
